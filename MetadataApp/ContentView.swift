cc3c6561160201a9e99576614ae824be307f0e13
Daniel <daniel@MacBook-Air.local>
2021-12-14 00:53:36 -0500

    Initial commit

---
+++ MetadataApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MetadataApp
+//
+//  Created by Daniel on 2021-12-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MetadataApp/MetadataAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetadataAppApp.swift
+//  MetadataApp
+//
+//  Created by Daniel on 2021-12-13.
+//
+
+import SwiftUI
+
+@main
+struct MetadataAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
